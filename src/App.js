dbc558d7d0c42937ad52bf27aaf106ec8ff124f9
PetruLucan <lucanpetru11@gmail.com>
2020-08-29 16:51:21 +0300

    borken homepage into smaller components and styled menu-items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
-import './hompage.styles.scss'
+import HomePage from './pages/homepage/homepage.component';
+import './pages/homepage/hompage.styles.scss'
 
 
 function App() {
