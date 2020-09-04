a72230265806c424aaca355d74f2c6b534f02781
PetruLucan <lucanpetru11@gmail.com>
2020-09-04 10:28:18 +0300

    update menu-items to navigate eithRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MenuItem from '../menu-item/menu-item.component.jsx';
 import './directory.styles.scss';
 
+
 class Directory extends React.Component {
     constructor() {
         super();
@@ -47,8 +48,8 @@ render (){
     return (
         <div className='directory-menu'>
             {
-                this.state.sections.map(({title, imageUrl, id, size}) =>(
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}></MenuItem>
+                this.state.sections.map(({id, ...otherSectionProps}) =>(
+                    <MenuItem key={id} {...otherSectionProps}></MenuItem>
                 ))
             }
         </div>
