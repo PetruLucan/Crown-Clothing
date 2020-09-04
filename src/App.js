3e27b2c1f10efd65b83b1427cbea2fcf820ea896
PetruLucan <lucanpetru11@gmail.com>
2020-09-04 14:53:53 +0300

    added collection-preview along with the homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
-import './pages/homepage/hompage.styles.scss'
+import ShopPage from './pages/shop/shop.component.jsx';
+import './pages/homepage/homepage.styles.scss';
+
 
 
 function App() {
   return (
     <div>
-    <HomePage/>;
+    <Switch>
+      <Route exact path='/' component={HomePage}></Route>
+      <Route path='/shop' component={ShopPage}></Route>
+    </Switch>
     </div>
   );
 }
