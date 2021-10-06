69e4ed368f83961990120a2c3018c725ecc12181
unknown <lucanpetru11@gmail.com>
2021-10-06 17:38:52 +0300

    configured redux-persist to use local storage to store cart data

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import {store, persistor} from './redux/store';
 
 import './index.css';
 import App from './App';
 
-import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <HashRouter>
       <React.StrictMode>
+        <PersistGate persistor={persistor}>
         <App />
+        </PersistGate>
       </React.StrictMode>
     </HashRouter>
   </Provider>,
